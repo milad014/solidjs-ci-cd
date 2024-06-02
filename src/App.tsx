ae32f7667f77c52aca3fd2d45e4d8d8912cb327b
milad <m.geravand@asax.ir>
2024-06-02 09:50:32 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Welcome To FrontEnd Chapter.
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
